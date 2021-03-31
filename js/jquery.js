56c1c1426f76674c525b30eaaad0949ec0dba53c
dmontejo20 <dmontejo@rubrika.es>
2021-03-31 14:33:27 +0200

    Cargar fonrt awesome

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+    let icono = document.createElement('i');
+
+    function ponflecha(){
+        $("nav > ol > li").append(icono);
+    }
+
+    // const ponflecha = function() {
+    //     $("nav > ol > li").append(icono);
+    // }
+    ponflecha();
     $("nav > ol > li").click(function(){
         $(this).children.toggleClass("activo");
     }); 
