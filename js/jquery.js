251cf984c7d509d3ee022b513b72e6cbee61adbc
dmontejo20 <dmontejo@rubrika.es>
2021-04-08 20:04:02 +0200

    rework

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,37 @@
 $(document).ready(function(){
+
+    //menú de la izquierda
     let icono = document.createElement('i');
 
     function ponflecha(){
-        $("nav > ol > li").append(icono);
+        $(".apartit").append(icono);
+        $(".apartit i").addClass("fas fa-angle-right");
     }
 
-    // const ponflecha = function() {
-    //     $("nav > ol > li").append(icono);
-    // }
     ponflecha();
-    $("nav > ol > li").click(function(){
-        $(this).children.toggleClass("activo");
-    }); 
+    $(".apartit").click(function(){
+        $(this).children(".fas").toggleClass("activo");
+        $(this).siblings(".subapartado").slideToggle("fast");
+    });
+
+    //menu de la derecha
+    function indice() {
+        let titulos = $("main h2");
+        let titulosArray = $.makeArray(titulos);
+
+        titulosArray = titulosArray.map((data , indice) => {
+
+            data.id="punto"+indice;
+            const resultado = { id:"punto"+indice, texto: $(data).html()}
+            return resultado
+        });
+
+        console.log(titulosArray);
+
+        $.each(titulosArray, function(i, val){
+            $(".indicecont ul").append(`<li class="punto"> <a href="#${val.id}" >  ${val.texto} </a> </li> `);
+        });
+        // $("indice ul").html(titulosArray);
+    }
+    indice();
 });
